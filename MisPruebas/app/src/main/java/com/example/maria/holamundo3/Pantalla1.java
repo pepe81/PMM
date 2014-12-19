0719489c27bb17546994f30083ce42a80d623193
pepe81 <jose.81@hotmail.com>
2014-12-19 08:53:01 +0100

    Añadidos proyectos

---
+++ MisPruebas/app/src/main/java/com/example/maria/holamundo3/Pantalla1.java
@@ -0,0 +1,130 @@
+package com.example.maria.holamundo3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class Pantalla1 extends Activity implements View.OnClickListener {
+    public static int COD_RESPUESTA=0;
+
+    TextView elSaludo;
+    TextView pantalla;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hola3);
+
+        final EditText miTexto= (EditText)findViewById(R.id.miTxt);
+        final Button miBoton= (Button)findViewById(R.id.miBtn);
+        elSaludo= (TextView)findViewById(R.id.miLbl);
+
+        //Si regreso de otra actividad
+        if (savedInstanceState!=null)
+        {
+            String mensajePasado=savedInstanceState.getString("TEXTO");
+            elSaludo.setText(mensajePasado);
+
+        }
+        //Borrar el texto inicial del EditText
+        miTexto.setOnFocusChangeListener(new View.OnFocusChangeListener()
+        {
+            public void onFocusChange(View v, boolean b)
+            {
+                if (b)  miTexto.setText("");
+
+            }
+        });
+
+        miBoton.setOnClickListener( new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent miIntent= new Intent(Pantalla1.this, Pantalla2.class);
+                Bundle miBundle=new Bundle();
+                String mensajePaso= "Te saludo " + miTexto.getText();
+                miBundle.putString("TEXTO", mensajePaso);
+                miIntent.putExtras(miBundle);
+                startActivityForResult(miIntent, COD_RESPUESTA);
+                //elSaludo.setText(mensajePaso);
+            }
+        });
+
+        Button boton1 = (Button) findViewById(R.id.miBtn1);
+        pantalla = (TextView)findViewById(R.id.tvColores);
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(),"Has pulsado el boton Rojo.",Toast.LENGTH_SHORT).show();
+                pantalla.setBackgroundColor(Color.RED);
+            }
+        });
+
+        Button boton3 = (Button)findViewById(R.id.miBtn3);
+        boton3.setOnClickListener(this);
+
+        Button btAceptar = (Button)findViewById(R.id.btAceptar);
+
+        btAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onCheckColor();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        pantalla = (TextView)findViewById(R.id.tvColores);
+        Toast.makeText(getBaseContext(),"Has pulsado el boton Verde.",Toast.LENGTH_SHORT).show();
+        pantalla.setBackgroundColor(Color.GREEN);
+    }
+
+    public void miClick(View v)
+    {
+        pantalla = (TextView)findViewById(R.id.tvColores);
+        Toast.makeText(getBaseContext(),"Has pulsado el boton Azul.",Toast.LENGTH_SHORT).show();
+        pantalla.setBackgroundColor(Color.BLUE);
+    }
+
+    public void onActivityResult(int cod_resp, int cod_result,Intent intent)
+    {
+        if (cod_result== RESULT_OK)
+        {
+            Bundle otroBundle = intent.getExtras();
+            elSaludo.setText(otroBundle.getString("DEVUELTO"));
+        }
+    }
+
+    public void onCheckColor ()
+    {
+        RadioButton op1 = (RadioButton)findViewById(R.id.op1);
+        RadioButton op2 = (RadioButton) findViewById(R.id.op2);
+        RadioButton op3 = (RadioButton) findViewById(R.id.op3);
+
+        if (op1.isChecked())
+        {
+            final TextView pantalla = (TextView)findViewById(R.id.tvColores);
+            pantalla.setBackgroundColor(Color.RED);
+        }
+        if (op2.isChecked())
+        {
+            final TextView pantalla = (TextView)findViewById(R.id.tvColores);
+            pantalla.setBackgroundColor(Color.BLUE);
+        }
+        if (op3.isChecked())
+        {
+            final TextView pantalla = (TextView)findViewById(R.id.tvColores);
+            pantalla.setBackgroundColor(Color.GREEN);
+        }
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/pepe/listview/MyActivity.java
@@ -0,0 +1,59 @@
+package com.example.pepe.listview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class MyActivity extends Activity {
+
+    String[]colores;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        colores = new String[]{"Rojo","Verde","Amarillo","Azul","Naranja"};
+
+        ArrayAdapter<String> adapter;
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,colores);
+        ListView listView=(ListView)findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),"Has pulsado el color: "+colores[position],Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
